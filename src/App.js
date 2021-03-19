39bb99ddc0814a32a7d1d3382dc609c590453c6b
Erik Portillo <erikportillo1@gmail.com>
2021-03-19 18:08:05 -0400

    Added Integration testing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./nprogress.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    eventCount: 5,
+  };
+  updateEvents = (location, eventCount) => {
+    let locationEvents;
+    getEvents().then((events) => {
+      if (location === "all" && eventCount === 0) {
+        locationEvents = events;
+      } else if (location !== "all" && eventCount === 0) {
+        locationEvents = events.filter((event) => event.location === location);
+        console.log(eventCount);
+      } else if (location === "" && eventCount > 0) {
+        locationEvents = events.slice(0, eventCount);
+      }
+      this.setState({
+        events: locationEvents,
+        eventCount,
+      });
+    });
+  };
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({
+          events: events.slice(0, this.state.eventCount),
+          locations: extractLocations(events),
+        });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.mounted = false;
+  }
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <NumberOfEvents />
-        <EventList />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+          eventCount={this.state.eventCount}
+        />
+        <NumberOfEvents
+          eventCount={this.state.eventCount}
+          updateEvents={this.updateEvents}
+        />
+        <EventList events={this.state.events} />
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { shallow } from "enzyme";
+import NumberOfEvents from "../NumberOfEvents";
+
+describe("<NumberOfEvents /> component", () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}} />);
+  });
+
+  test("render text input", () => {
+    expect(NumberOfEventsWrapper.find(".event-number-input")).toHaveLength(1);
+  });
+
+  test("check input default value is equal to undefined", () => {
+    expect(
+      NumberOfEventsWrapper.find(".event-number-input").at(0).props().value
+    ).toEqual(undefined);
+  });
+
+  test("check placeholder to be Enter number of events", () => {
+    expect(
+      NumberOfEventsWrapper.find(".event-number-input").at(0).props()
+        .placeholder
+    ).toEqual("Enter Number of Events");
+  });
+
+  test("check on change state", () => {
+    const eventCount = { target: { value: 8 } };
+
+    NumberOfEventsWrapper.find(".event-number-input").simulate(
+      "change",
+      eventCount
+    );
+    expect(NumberOfEventsWrapper.state("eventCount")).toBe(8);
+  });
+
+  test("check type is equal to number", () => {
+    expect(
+      NumberOfEventsWrapper.find(".event-number-input").at(0).props().type
+    ).toEqual("number");
+  });
+});
